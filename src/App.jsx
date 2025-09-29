f865dc23430e58f9e8287d12648788150da32ff3
Kevin Jagdeo <70348657+kevinjagdeo@users.noreply.github.com>
2025-09-29 02:12:21 +0000

    feat: added signup page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { Blog } from './Blog.jsx'
+import { Blog } from './pages/Blog.jsx'
+import { Signup } from './pages/Signup.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 const queryClient = new QueryClient()
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Blog />,
+  },
+  {
+    path: '/signup',
+    element: <Signup />,
   },
 ])
 export function App() {
