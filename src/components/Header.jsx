c467325c1b186296a8a21315f63f9917abd4e218
Kevin Jagdeo <70348657+kevinjagdeo@users.noreply.github.com>
2025-09-29 02:29:40 +0000

    feat: added ability to login and logout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,27 @@
 import { Link } from 'react-router-dom'
+import { jwtDecode } from 'jwt-decode'
+import { useAuth } from '../contexts/AuthContext.jsx'
+
 export function Header() {
+  const [token, setToken] = useAuth()
+  if (token) {
+    const { sub } = jwtDecode(token)
+    return (
+      <div>
+        <h1>Welcome to My Blog!</h1>
+        Logged in as <b>{sub}</b>
+        <br />
+        <button onClick={() => setToken(null)}>Logout</button>
+      </div>
+    )
+  }
+
   return (
     <div>
       <h1>Welcome to My Blog!</h1>
-
-      <Link to='/signup'>Sign Up</Link>
+      <div>
+        <Link to='/login'>Log In</Link> | <Link to='/signup'>Sign Up</Link>
+      </div>
     </div>
   )
 }
