bfb34a693788e8d89f4deb6ec1082081224e5b78
Kevin Jagdeo <70348657+kevinjagdeo@users.noreply.github.com>
2025-10-21 02:38:52 +0000

    chore: added dynamic titles

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -3,6 +3,7 @@ import { CreatePost } from '../components/CreatePost.jsx'
 import { PostFilter } from '../components/PostFilter.jsx'
 import { PostSorting } from '../components/PostSorting.jsx'
 import { Header } from '../components/Header.jsx'
+import { Helmet } from 'react-helmet-async'
 import { useState } from 'react'
 import { useQuery } from '@tanstack/react-query'
 import { getPosts } from '../api/posts.js'
@@ -18,6 +19,9 @@ export function Blog() {
   const posts = postsQuery.data ?? []
   return (
     <div style={{ padding: 8 }}>
+      <Helmet>
+        <title>Full-Stack React Blog</title>
+      </Helmet>
       <Header />
       <br />
       <hr />
