5c6704e275098bf0f6d57a4d3f56bb2ef45f4726
Kevin Jagdeo <70348657+kevinjagdeo@users.noreply.github.com>
2025-11-13 03:57:08 +0000

    feat: create UI for chat in react

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,15 @@
 import { Link } from 'react-router-dom'
-import { User } from './User.jsx'
 import { jwtDecode } from 'jwt-decode'
+import { User } from './User.jsx'
+import { useSocket } from '../contexts/SocketIOContext.jsx'
 import { useAuth } from '../contexts/AuthContext.jsx'
-
 export function Header() {
   const [token, setToken] = useAuth()
+  const { socket } = useSocket()
+  const handleLogout = () => {
+    socket.disconnect()
+    setToken(null)
+  }
   if (token) {
     const { sub } = jwtDecode(token)
     return (
@@ -12,17 +17,14 @@ export function Header() {
         <h1>Welcome to My Blog!</h1>
         Logged in as <User id={sub} />
         <br />
-        <button onClick={() => setToken(null)}>Logout</button>
+        <button onClick={handleLogout}>Logout</button>
       </div>
     )
   }
-
   return (
     <div>
       <h1>Welcome to My Blog!</h1>
-      <div>
-        <Link to='/login'>Log In</Link> | <Link to='/signup'>Sign Up</Link>
-      </div>
+      <Link to='/login'>Log In</Link> | <Link to='/signup'>Sign Up</Link>
     </div>
   )
 }
