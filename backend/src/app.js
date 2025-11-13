d19490b22a75f0eb307a15bccb2979e666f3dbb8
Kevin Jagdeo <70348657+kevinjagdeo@users.noreply.github.com>
2025-11-13 03:16:14 +0000

    chore: removed blog components

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { postsRoutes } from './routes/posts.js'
+//import { postsRoutes } from './routes/posts.js'
 import { userRoutes } from './routes/users.js'
 import bodyParser from 'body-parser'
 import cors from 'cors'
@@ -10,7 +10,7 @@ import { handleSocket } from './socket.js'
 
 app.use(bodyParser.json())
 app.use(cors())
-postsRoutes(app)
+//postsRoutes(app)
 userRoutes(app)
 app.get('/', (req, res) => {
   res.send('Hello from Atlas!!')
