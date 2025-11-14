23a3b7fe495f1aec63b6685bf831c1583aa8140e
Kevin Jagdeo <70348657+kevinjagdeo@users.noreply.github.com>
2025-11-14 01:44:32 +0000

    feat: added ability to store and replay messages using mongo

--- src/components/ChatMessage.jsx
+++ src/components/ChatMessage.jsx
@@ -1,12 +1,19 @@
 import PropTypes from 'prop-types'
-export function ChatMessage({ username, message }) {
+export function ChatMessage({ username, message, replayed }) {
   return (
-    <div>
-      <b>{username}</b>: {message}
+    <div style={{ opacity: replayed ? 0.5 : 1.0 }}>
+      {username ? (
+        <span>
+          <b>{username}</b>: {message}
+        </span>
+      ) : (
+        <i>{message}</i>
+      )}
     </div>
   )
 }
 ChatMessage.propTypes = {
-  username: PropTypes.string.isRequired,
+  username: PropTypes.string,
   message: PropTypes.string.isRequired,
+  replayed: PropTypes.bool,
 }
